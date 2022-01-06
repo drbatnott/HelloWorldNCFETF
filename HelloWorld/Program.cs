3decc939081fa499cd187fe909a02bca322ef148
Peter Blanchfield <zygomy@gmail.com>
2022-01-06 15:03:33 +0000

    My first Code Tests for the project Just enter data but don't save it

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, please enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.Write("What is your age? ");
+            int age;
+            age = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("OK " + name + " is your age really " + age + "?");
+            float weight;
+            Console.Write("OK " + name + " what is your weight? ");
+            weight = (float)Double.Parse(Console.ReadLine());
+            Console.WriteLine("OK " + name + " is your weight really " + weight + "?");
+            Console.ReadLine();
+
+        }
+    }
+}
